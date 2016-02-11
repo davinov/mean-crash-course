591f2235afdc9ceae1508c125fa896315adb2c3a
David Nowinsky <david@nowinsky.net>
2016-02-11 16:21:27 +0100

    Created AttendeesController

--- static/script.js
+++ static/script.js
@@ -1,2 +1,8 @@
 // Creation of my new module
 angular.module('mean-course', []);
+
+// My first controller
+angular.module('mean-course')
+.controller('AttendeesController', function() {
+  console.log('This is my new AttendeesController!')
+});
