b86cbec1eb9b1dd61cb897bb5b3920edcc23d4b5
David Nowinsky <david@nowinsky.net>
2016-02-11 16:11:33 +0100

    Add static folder and index page

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Server is up!');
-});
+app.use(express.static('static'));
 
 var port = 3000;
 app.listen(port, function () {
