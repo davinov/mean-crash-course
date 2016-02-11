36ec706cbb16b09454bd6ab8be14e9da5a85537b
David Nowinsky <david@nowinsky.net>
2016-02-11 15:55:29 +0100

    Add up server message

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Server is up!');
+});
+
 var port = 3000;
 app.listen(port, function () {
   console.log('Server is listening on port ' + port);
