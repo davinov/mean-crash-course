8219c0db3089e093842bb1150502d9c883c66fdc
David Nowinsky <david@nowinsky.net>
2016-02-11 17:49:52 +0100

    Simple chart based on eye colors data

--- static/script.js
+++ static/script.js
@@ -15,6 +15,36 @@ angular.module('mean-course')
       .map('eye-color')
       .uniq()
       .value();
+
+    // Prepare eyeColors chart data
+    eyesColorData = d3.nest()
+      .key(function (d) { return d['eye-color']; })
+      .entries($scope.attendees);
+    // See the result in the console:
+    console.log(eyesColorData);
+
+    // Taken from nvD3 and customized
+    nv.addGraph(function() {
+      var chart = nv.models.discreteBarChart()
+        .x(function(d) { return d.key; })
+        .y(function(d) { return d.values.length; })
+        .margin({top: 30, right: 20, bottom: 50, left: 175})
+        .showValues(true);
+
+      chart.yAxis
+        .tickFormat(d3.format(',.0f'));
+
+      d3.select('.attendees-list svg')
+        .datum([{
+          key: 'Eye colors',
+          values: eyesColorData
+        }])
+        .call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
   })
   // Handle the error case
   .catch(function () {
